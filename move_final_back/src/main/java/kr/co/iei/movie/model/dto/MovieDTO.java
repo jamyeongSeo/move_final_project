2182c2143a7a2b8b9934993cbcdb221be814e8af
simbacode6632 <simba6632@gmail.com>
2025-10-14 18:06:43 +0900

    MovieDTO 작성 커밋

---
+++ move_final_back/src/main/java/kr/co/iei/movie/model/dto/MovieDTO.java
@@ -0,0 +1,28 @@
+package kr.co.iei.movie.model.dto;
+
+import java.sql.Date;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Alias(value="movie")
+public class MovieDTO {
+	private int movieNo;
+	private int movieType;
+	private String movieTitle;
+	private int movieStatus;
+	private String movieContent;
+	private String movieThumb;
+	private int movieGenre;
+	private int movieGrade;
+	private int movieRuntime;
+	private String movieDirector;
+	private String movieActor;
+	private Date movieRelease;
+}
