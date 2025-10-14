0ededa254cd1bd8e10e1e29e53cc2950938f2e15
simbacode6632 <simba6632@gmail.com>
2025-10-14 11:01:54 +0900

    백엔드 예매파트 패키지 클래스 생성

---
+++ move_final_back/src/main/java/kr/co/iei/booking/model/dto/BookingDTO.java
@@ -0,0 +1,5 @@
+package kr.co.iei.booking.model.dto;
+
+public class BookingDTO {
+
+}
---
+++ move_final_front/src/component/booking/BookingMain.jsx
@@ -0,0 +1,13 @@
+const BookingMain = () => {
+  return (
+    <div>
+      <div className="book-all-wrap">
+        <div className="page-title">
+          <h2>예매하기</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookingMain;
