92dff39b23ceacf1a7d239a1b2f1a5c0f6f0d061
jamyeong <seojamyeong@gmail.com>
2025-10-14 10:38:11 +0900

    (back)기본 셋팅

---
+++ move_final_back/src/main/java/kr/co/iei/MoveFinalBackApplication.java
@@ -0,0 +1,14 @@
+package kr.co.iei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class MoveFinalBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveFinalBackApplication.class, args);
+	}
+
+}
