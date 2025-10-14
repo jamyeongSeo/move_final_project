a63e17ef318b59c2e4a7177b5c31367d750dab0a
jamyeong <seojamyeong@gmail.com>
2025-10-14 10:23:27 +0900

    (front)기본 셋팅

---
+++ move_final_front/src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="wrap">
+      <main className="content">
+        <Routes></Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
