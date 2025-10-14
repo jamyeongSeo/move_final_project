e65a53f623db5ff00a480331bb0489c34319fc6f
chans0928 <chans0928@gmail.com>
2025-10-14 11:16:02 +0900

    헤더/푸터 및 고객센터 기본 front 작업

---
+++ move_final_front/src/component/cs/CSMain.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import LeftSideMenu from "../utils/LeftSideMenu";
+import { Route, Routes } from "react-router-dom";
+import NoticeList from "./NoticeList";
+import "./cs.css";
+
+const CSMain = () => {
+  const [menus, setMenus] = useState([
+    { url: "/cs/notice", text: "공지사항" },
+    { url: "/cs/faq", text: "FAQ" },
+    { url: "/cs/pq", text: "1:1 문의" },
+  ]);
+  return (
+    <div className="cs-wrap">
+      <div className="cs-side">
+        <section className="section side-menu-box">
+          <div>고객센터</div>
+        </section>
+        <section className="section">
+          <LeftSideMenu menus={menus} />
+        </section>
+      </div>
+      <div className="cs-content">
+        <section className="section">
+          <Routes>
+            <Route path="notice" element={<NoticeList />} />
+          </Routes>
+        </section>
+      </div>
+    </div>
+  );
+};
+export default CSMain;
