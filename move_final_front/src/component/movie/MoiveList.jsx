54c4123efca2d42b25d29fa13682210ef621dd32
simbacode6632 <simba6632@gmail.com>
2025-10-15 09:41:59 +0900

    영화 리스트 front 작업 1

---
+++ move_final_front/src/component/movie/MoiveList.jsx
@@ -0,0 +1,14 @@
+const MovieList = () => {
+  return (
+    <div className="content">
+      <div className="content-wrap">
+        <div className="movie-header">
+          <h2 className="page-title">영화 리스트</h2>
+        </div>
+        <div className="movie-list-box"></div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieList;
