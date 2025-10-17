6f713e9e3ff16498c2cc489db3c2b25a86d60d25
moonzi <yunji6834@gmail.com>
2025-10-17 10:05:59 +0900

    adminsidemenu수정중

--- move_final_back/src/main/java/kr/co/iei/admin/controller/AdminController.java
+++ move_final_back/src/main/java/kr/co/iei/admin/controller/AdminController.java
@@ -1,18 +1,27 @@
 package kr.co.iei.admin.controller;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import kr.co.iei.admin.model.service.AdminService;
 import kr.co.iei.movie.model.dto.MovieDTO;
+import kr.co.iei.movie.model.dto.MovieFileDTO;
+import kr.co.iei.utils.FileUtil;
 
 @CrossOrigin("*")
 @RestController
@@ -20,13 +29,38 @@ import kr.co.iei.movie.model.dto.MovieDTO;
 public class AdminController {
 	@Autowired
 	private AdminService adminService;
-	
+	@Autowired
+	private FileUtil fileUtil;
+	@Value("${file.root}")
+	private String root;
+
 	@GetMapping(value="/movie")
 	public ResponseEntity<Map> adminMovieList(@RequestParam int reqPage){
 		Map map = adminService.adminMovieList(reqPage);
 		return ResponseEntity.ok(map);
-		
 	}
+	
+	@PostMapping(value="/image")
+	public ResponseEntity<String> editorImageUpload(@ModelAttribute MultipartFile image){
+		String savepath = root + "/editor/";
+		String filepath = fileUtil.upload(savepath, image);
+		return ResponseEntity.ok(filepath);			
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Integer> insertMoive(@ModelAttribute MovieDTO movie,
+												@ModelAttribute MultipartFile movieThumb){
+		List<MovieDTO> movieInfoList = new ArrayList<MovieDTO>();
+		String savepath = root +"/thumb/";
+		String filepath = fileUtil.upload(savepath, movieThumb);
+		String filename = movieThumb.getOriginalFilename();
+		
+			
+		}
+		
+	
+	
+	
 	
 	
 }
--- move_final_front/src/component/admin/AdminMain.jsx
+++ move_final_front/src/component/admin/AdminMain.jsx
@@ -5,25 +5,8 @@ import Swal from "sweetalert2";
 import axios from "axios";
 //import LeftSideMenu from "../utils/LeftSideMenu";
 import "./admin.css";
+import AdminSideMenu from "./AdminSideMenu";
 
-const subMenus = {
-  "/admin/main": [
-    { text: "영화 목록", url: "/admin/movie/list" },
-    { text: "영화 등록", url: "/admin/movie/regist" },
-    { text: "스케줄 등록", url: "/admin/schedule/regist" },
-    { text: "관객 수 조회", url: "/admin/movieGoer" },
-  ],
-  "admin/sales": [
-    { text: "전체 매출 조회", url: "/admin/salesAll" },
-    { text: "각 영화 매출 조회", url: "/admin/salesOne" },
-  ],
-};
-
-{
-  /* 
- 
-*/
-}
 
 const AdminMain = () => {
   const menus = [
@@ -31,6 +14,18 @@ const AdminMain = () => {
     { url: "/admin/sales", text: "매출 관리" },
     { url: "/admin/member", text: "회원 관리" },
   ];
+  const subMenus = {
+    "/admin/main": [
+      { text: "영화 목록", url: "/admin/movie/list" },
+      { text: "영화 등록", url: "/admin/movie/regist" },
+      { text: "스케줄 등록", url: "/admin/schedule/regist" },
+      { text: "관객 수 조회", url: "/admin/movieGoer" },
+    ],
+    "/admin/sales": [
+      { text: "전체 매출 조회", url: "/admin/salesAll" },
+      { text: "각 영화 매출 조회", url: "/admin/salesOne" },
+    ],
+  };
   const [openMenu, setOpenMenu] = useState(null);
   const menuClick = (menuUrl) => {
     setOpenMenu((prev) => (prev === menuUrl ? null : menuUrl));
@@ -57,38 +52,11 @@ const AdminMain = () => {
   }
 
   return (
-    <section className="admin-list-wrap">
-      <div className="side-wrap">
-        <section className="side-menu-box">
-          <div>관리자 페이지</div>
-        </section>
-
-        {/*{menus.map((menu, index) => (
-            <div key={index}>
-              <div onClick={() => menuClick(menu.url)}>
-              </div>
-              {openMenu === menu.url && (
-                <ul className="sub-menu-list">
-                  {subMenus[menu.url]?.map((sub, i) => (
-                    <li key={`sub-${i}`}>
-                      <NavLink
-                        to={subMenus.url}
-                        className={({ isActive }) =>
-                          isActive ? "active-link" : ""
-                        }
-                      >
-                        {sub.text}
-                      </NavLink>
-              
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </div>
-          ))}
-                      */}
+    <div className="admin-list-wrap">
+      <AdminSideMenu menus={menus} subMenus={subMenus} />
+      <div className="admin-main-content">
       </div>
-    </section>
+    </div>
   );
 };
 
